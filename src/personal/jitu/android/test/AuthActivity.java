0cb9c029b63f51f4e752552605dc4d1906e3b372
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-05-04 12:12:55 -0400

    Uploaded sample application with google authentication

---
+++ src/personal/jitu/android/test/AuthActivity.java
@@ -0,0 +1,133 @@
+package personal.jitu.android.test;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AuthActivity extends Activity {
+	private static final String ACCOUNT_TYPE = "com.google"; //com.facebook.auth.login
+	private static final String AUTH_TOKEN_TYPE = "lh2";
+	private static final int DIALOG_ACCOUNTS = 0;
+	private static final String PREF = "MyPrefs";
+	private String authToken;
+	private static final int REQUEST_AUTHENTICATE = 0;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		gotAccount(false);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DIALOG_ACCOUNTS:
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Select a Google account");
+			final AccountManager manager = AccountManager.get(this);
+			final Account[] accounts = manager.getAccountsByType(ACCOUNT_TYPE);
+			final int size = accounts.length;
+			String[] names = new String[size];
+			for (int i = 0; i < size; i++) {
+				names[i] = accounts[i].name;
+			}
+			builder.setItems(names, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					gotAccount(manager, accounts[which]);
+				}
+			});
+			return builder.create();
+		}
+		return null;
+	}
+
+	private void gotAccount(boolean tokenExpired) {
+		SharedPreferences settings = getSharedPreferences(PREF, 0);
+		String accountName = settings.getString("accountName", null);
+		if (accountName != null) {
+			AccountManager manager = AccountManager.get(this);
+			Account[] accounts = manager.getAccountsByType(ACCOUNT_TYPE);
+			int size = accounts.length;
+			for (int i = 0; i < size; i++) {
+				Account account = accounts[i];
+				if (accountName.equals(account.name)) {
+					if (tokenExpired) {
+						manager.invalidateAuthToken(ACCOUNT_TYPE, this.authToken);
+					}
+					gotAccount(manager, account);
+					return;
+				}
+			}
+		}
+		showDialog(DIALOG_ACCOUNTS);
+	}
+
+	private void gotAccount(final AccountManager manager, final Account account) {
+		SharedPreferences settings = getSharedPreferences(PREF, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("accountName", account.name);
+		editor.commit();
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					final Bundle bundle = manager.getAuthToken(account, AUTH_TOKEN_TYPE, true, null, null).getResult();
+					runOnUiThread(new Runnable() {
+
+						public void run() {
+							try {
+								if (bundle.containsKey(AccountManager.KEY_INTENT)) {
+									Intent intent = bundle.getParcelable(AccountManager.KEY_INTENT);
+									int flags = intent.getFlags();
+									flags &= ~Intent.FLAG_ACTIVITY_NEW_TASK;
+									intent.setFlags(flags);
+									startActivityForResult(intent, REQUEST_AUTHENTICATE);
+								} else if (bundle.containsKey(AccountManager.KEY_AUTHTOKEN)) {
+									authenticatedClientLogin(bundle.getString(AccountManager.KEY_AUTHTOKEN));
+								}
+							} catch (Exception e) {
+								handleException(e);
+							}
+						}
+					});
+				} catch (Exception e) {
+					handleException(e);
+				}
+			}
+		}.start();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case REQUEST_AUTHENTICATE:
+			if (resultCode == RESULT_OK) {
+				gotAccount(false);
+			} else {
+				showDialog(DIALOG_ACCOUNTS);
+			}
+			break;
+		}
+	}
+
+	private void authenticatedClientLogin(String authToken) {
+		this.authToken = authToken;
+		Toast.makeText(this, authToken, Toast.LENGTH_LONG).show();
+//		((GoogleHeaders) transport.defaultHeaders).setGoogleLogin(authToken);
+	}
+
+	private void handleException(Exception e) {
+		e.printStackTrace();
+	}
+}
